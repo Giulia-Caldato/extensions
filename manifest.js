5fda5a2339af64e7a21650d651b954fbb89e6b55
Giulia-Caldato <76591550+Giulia-Caldato@users.noreply.github.com>
2021-05-16 18:32:26 +0200

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,11 @@
+{
+    "manifest_version": 3,
+    "name": "Carbon Footprint",
+    "version": "1.0.0",
+    "host_permissions": ["<all_urls>"],
+    "permissions": ["activeTab", "scripting", "storage"],
+    "action": {
+        "default_popup": "index.html"
+    }
+
+object.style.backgroundColor="#00FF00"
